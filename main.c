22499411bf977c50c730b505c304469126adebf9
Cosmin Stretea <cosmin.stretea@gmail.com>
2019-11-10 21:56:06 +0200

    last updates

--- main.c
+++ main.c
@@ -11,9 +11,9 @@ int main() {
     int Food, Type, Drink, Cutlery;
     int state = 0;
     int orderPlaced = 0;
-    int noOfFoodTypes = 3, noOfDrinks = 4, noOfDrinkOptions = 5, noOfCutleryOptions = 2;
+    int NoOfFoodTypes = 3, NoOfDrinks = 4, NoOfDrinkOptions = 5, NoOfCutleryOptions = 2;
     double DrinkPrice[] = {5,5,5,4};
-    int noOfFoodOptions[3] = {3,2,4};
+    int NoOfFoodOptions[3] = {3,2,4};
     char FoodType[3][10] = {"Pizza", "Pasta", "Salad"};
     char Drinks[4][10] = {"Coca-Cola", "Fanta", "Lipton", "Water"};
     char FoodOption[3][4][50] = {
@@ -34,25 +34,25 @@ int main() {
                 break;
             }
             case(1):{
-                printFoodTypes(noOfFoodTypes, FoodType);
-                Food = getChoiceIndex(noOfFoodTypes, &state);
+                printFoodTypes(NoOfFoodTypes, FoodType);
+                Food = getChoiceIndex(NoOfFoodTypes, &state);
                 break;
             }
             case(2):{
                 printf("Please choose the type of %s:\n", FoodType[Food]);
-                printFoodOptions(noOfFoodOptions, FoodPrice, FoodOption, Food);
-                Type = getChoiceIndex(noOfFoodOptions[Food], &state);
+                printFoodOptions(NoOfFoodOptions, FoodPrice, FoodOption, Food);
+                Type = getChoiceIndex(NoOfFoodOptions[Food], &state);
                 break;
             }
             case(3):{
                 printf("Please choose a drink to go with your %s:\n", FoodType[Food]);
-                printDrinkOptions(noOfDrinks, DrinkPrice, Drinks);
-                Drink = getChoiceIndex(noOfDrinkOptions, &state);
+                printDrinkOptions(NoOfDrinks, DrinkPrice, Drinks);
+                Drink = getChoiceIndex(NoOfDrinkOptions, &state);
                 break;
             }
             case(4):{
                 printCutleryOptions();
-                Cutlery = getChoiceIndex(noOfCutleryOptions, &state);
+                Cutlery = getChoiceIndex(NoOfCutleryOptions, &state);
                 break;
             }
             case(5):{
@@ -61,17 +61,8 @@ int main() {
                 break;
             }
             case(6):{
-                printOrderHeader(username);
-                printOrderFood( FoodOption, FoodPrice, Food, Type);
-                printOrderDrinks(Drinks, DrinkPrice, Drink, noOfDrinks);
-                printOrderCutlery(Cutlery);
-                printAddInfo(AddInfo);
-                double total = 0;
-                if(Drink == noOfDrinks)
-                    total = FoodPrice[Food][Type];
-                else
-                    total = FoodPrice[Food][Type] + DrinkPrice[Drink];
-                printOrderTotal(total);
+                printOrder( username, FoodOption, FoodPrice, Drinks,
+                 DrinkPrice, AddInfo, Cutlery, Food, Type, Drink, NoOfDrinks);
                 orderConfirmation(&state, &orderPlaced);
                 break;
             }
